ab1ba7a2fa2dfc6212795e14e532ceb4aa622d41
spark <zxp6097@g.rit.edu>
2019-05-23 14:13:22 +0200

    test

--- src/views/Home.js
+++ src/views/Home.js
@@ -27,53 +27,53 @@ class Home extends Component {
     getUserData = () => {
 
         let something = fire;
-        
+
         let database = something.database();
 
         let ref = database.ref('items');
 
         ref.on('value', this.gotData, this.errData);
-        
-      }
 
-      gotData = (data) => {
-          console.log('data retrieved')
-          console.log(data.val());
-      }
-
-      errData = (err) => {
-          console.log(err);
-          console.log('error');
-      }
-
-      componentDidMount() {
-        this.getUserData();
-      }
-
-      addItem = (e) => {
-          e.preventDefault();
-
-            let id = e.target.elements.id.value;
-            let name = e.target.elements.name.value;
-            let description = e.target.elements.description.value;
-            let category = e.target.elements.category.value;
-            let barcode = e.target.elements.barcode.value;
-
-        this.writeUserData(id,name,description,category,barcode) 
-        
     }
 
-      writeUserData = (id,name,description,category,barcode) => {
+    gotData = (data) => {
+        console.log('data retrieved')
+        console.log(data.val());
+    }
+
+    errData = (err) => {
+        console.log(err);
+        console.log('error');
+    }
+
+    componentDidMount() {
+        this.getUserData();
+    }
+
+    addItem = (e) => {
+        e.preventDefault();
+
+        let id = e.target.elements.id.value;
+        let name = e.target.elements.name.value;
+        let description = e.target.elements.description.value;
+        let category = e.target.elements.category.value;
+        let barcode = e.target.elements.barcode.value;
+
+        this.writeUserData(id, name, description, category, barcode)
+
+    }
+
+    writeUserData = (id, name, description, category, barcode) => {
         fire.database().ref('items/' + id).set({
             name: name,
             description: description,
             category: category,
             barcode: barcode
         });
-      }
+    }
     render() {
         return (
-        <div>   
+            <div>
                 <form onSubmit={this.addItem}>
                     <input type="text" name="id"></input>
                     <input type="text" name="name"></input>
@@ -82,8 +82,8 @@ class Home extends Component {
                     <input type="text" name="barcode"></input>
                     <input type="submit" value="Add"></input>
                 </form>
-            
-                <h1>Welcome to Home</h1>
+
+                <h1>Welcome to Home page</h1>
                 <button onClick={this.logout}>Logout</button>
             </div>
         );
